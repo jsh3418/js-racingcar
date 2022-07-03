6c8452c5049bd24ad2be79d554a659e49f574e8b
jsh3418 <albert5428@gmail.com>
2022-07-03 16:23:44 +0900

    fix: 시도 횟수 유효성 검사 강화
    시도 횟수 빈칸으로 입력해도 진행됐었으나 fix
    시도 횟수를 소수로 입력해도 진행됐었으나 막음

--- js/controller/handleRaceTimeInput.js
+++ js/controller/handleRaceTimeInput.js
@@ -6,12 +6,18 @@ export const handleRaceTimeInput = (event) => {
   const raceTimeInput = document.querySelector("#raceTimeInput");
   const raceTimeSubmitButton = document.querySelector("#raceTimeSubmitButton");
 
-  store.raceTime = parseInt(raceTimeInput.value, 10);
-  if (store.raceTime <= 0) {
+  let raceTime = raceTimeInput.value;
+  if (raceTime <= 0 || raceTime === "") {
     alert("시도 횟수는 1이상 입력해야 합니다.");
     document.querySelector("#raceTimeInput").value = "";
     return;
   }
+  if (raceTime.includes(".") === true) {
+    alert("시도 횟수는 1이상인 정수로 입력해주세요.");
+    document.querySelector("#raceTimeInput").value = "";
+    return;
+  }
+  store.raceTime = raceTime;
 
   raceTimeInput.disabled = true;
   raceTimeSubmitButton.disabled = true;
